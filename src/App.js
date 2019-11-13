a812d5880c902a6486b9a566a43cb22cce686344
barema4 <samrubarema6@gmail.com>
2019-11-13 16:37:52 +0300

    Finished home page, styling of the menu items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/HomePage'
+
 function App() {
   return (
     <div>
+      <HomePage/>
     
     </div>
   );
