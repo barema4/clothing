a77094c3134d14335eefaafd4346d458f0639944
barema4 <samrubarema6@gmail.com>
2019-11-18 12:53:52 +0300

    adding header to shop page , contact

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Shop from './pages/shop/Shop'
 import './App.css';
 import HomePage from './pages/homepage/HomePage'
+import Header from '../src/components/header/Header'
 
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={Shop} />
